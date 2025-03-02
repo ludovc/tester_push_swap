99122291f358c8a89754991fe7210715660550bf
ludovc <ludovcasu@gmail.com>
2025-03-03 00:32:39 +0100

    wip

--- tester.sh
+++ tester.sh
@@ -1,11 +1,14 @@
-args=""
-output=$(../push_swap $args)
-printf "%s\n" "$output" | ./checker $args
+test() {
+	if [ -z "$1" ]; then
+		output=$(../push_swap)
+		[ -z "$output" ] && echo OK || echo KO
+	else
+		args=$1
+		output=$(../push_swap $args)
+		printf "%s\n" "$output" | ./checker $args
+	fi
+}
 
-args="2 1"
-output=$(../push_swap $args)
-printf "%s\n" "$output" | ./checker $args
-
-args="324 234 325 34 3 1"
-output=$(../push_swap $args)
-printf "%s\n" "$output" | ./checker $args
+test ""
+test "2 1"
+test "324 234 325 34 3 1"
