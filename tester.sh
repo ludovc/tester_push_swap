f0005f79b75d44d940c248a642a2855ef7945daa
ludovc <ludovcasu@gmail.com>
2025-03-03 00:39:45 +0100

    wip

--- tester.sh
+++ tester.sh
@@ -1,10 +1,10 @@
 test() {
 	if [ -z "$1" ]; then
-		output=$(../push_swap)
+		output=$(../push_swap 2>&1)
 		[ -z "$output" ] && echo OK || echo KO
 	else
 		args=$1
-		output=$(../push_swap $args)
+		output=$(../push_swap $args 2>&1)
 		printf "%s\n" "$output" | ./checker $args
 	fi
 }
