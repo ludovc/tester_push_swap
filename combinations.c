9615194b4adcca7436860c463da42bcc371e69ab
ludovc <ludovcasu@gmail.com>
2025-04-03 13:34:27 +0200

    combinations.c

---
+++ combinations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void	combinations()
+{
+	int		a;
+	int		b;
+
+	a = 1;
+	while (a <= 2)
+	{
+		b = 1;
+		while (b <= 2)
+		{
+			printf("%d %d\n", a, b);
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main()
+{
+	//printf("1 2\n");
+	//printf("2 1\n");
+
+	combinations();
+}
